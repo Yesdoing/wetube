b9aa8134ccd52273eacbe01100f765864a80c504
Jane Lee <Yesdoing13@gmail.com>
2019-04-25 00:27:39 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ function handleListening() {
     console.log(`Listening on > http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+    res.send("Hello~ This is Home~");
+};
+
+function handleProfile(req, res) {
+    res.send("Okay This is Profile Page!!");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
