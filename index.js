3025304ba4cd0f47c2d51cc54974c4d0499b5354
Jane Lee <Yesdoing13@gmail.com>
2019-04-25 01:11:37 +0900

    Express Core: MiddleWare Part Two

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
 const app = express();
 const PORT = 4000;
 
@@ -13,12 +18,12 @@ const handleProfile = (req, res) => {
   res.send("Okay This is Profile Page!!");
 };
 
-const handleBetween = (req, res, next) => {
-    console.log("Between!!");
-    next();
-}
 
-app.use(handleBetween);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
 
 app.get("/", handleHome);
 
