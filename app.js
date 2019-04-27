a5644fdba11eb2f89dd900faae81afe3801645cf
Jane Lee <Yesdoing13@gmail.com>
2019-04-27 16:34:16 +0900

    code refactoring

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import { userRouter } from './routers/userRouter';
 
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 
-import { routes } from './routes' 
+import routes from './routes' 
 
 const app = express();
 
@@ -21,7 +20,7 @@ app.use(morgan('dev'));
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
-app.user(routes.videos, videoRouter);
+app.use(routes.videos, videoRouter);
 
 
 export default app;
---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+export const join = (req, res) => res.send('Join');
+export const login = (req, res) => res.send('Login');
+export const logout = (req, res) => res.send('Logout');
+
+export const users = (req, res) => res.send('users');
+export const userDetail = (req, res) => res.send('userDetail');
+export const editProfile = (req, res) => res.send('editProfile');
+export const changePassword = (req, res) => res.send('changePassword');
