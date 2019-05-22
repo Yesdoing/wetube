54af70003fdf8635f54461544d3e075d27953456
Jane Lee <Yesdoing13@gmail.com>
2019-05-22 18:28:26 +0900

    Connection to mongodb

--- db.js
+++ db.js
@@ -1,50 +1,16 @@
-export const videos = [
+import mongoose from 'mongoose';
+
+mongoose.connect(
+    "mongodb://localhost:27017/wetube",
     {
-        id: 39129,
-        title: "Awesome Video",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 12321,
-            name: "Yesdoing",
-            email: "fkrmtleo12@gmail.com"
-        }
-    },
-    {
-        id: 1212121,
-        title: "Video super",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-          id: 121212,
-          name: "Nicolas",
-          email: "nico@las.com"
-        }
-      },
-      {
-        id: 55555,
-        title: "Video nice",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-          id: 121212,
-          name: "Nicolas",
-          email: "nico@las.com"
-        }
-      },
-      {
-        id: 11111,
-        title: "Video perfect",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-          id: 121212,
-          name: "Nicolas",
-          email: "nico@las.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-]
+)
+
+const db = mongoose.connection;
+const handleOpen = () => console.log("✅  Connected To DB");
+const handleError = (error) => console.log(`❌ Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
