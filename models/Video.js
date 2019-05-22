c55458cdd05fba3777cfe78eef6a7a88b0030197
Jane Lee <Yesdoing13@gmail.com>
2019-05-22 21:19:56 +0900

    Comment Model

--- models/Video.js
+++ models/Video.js
@@ -17,7 +17,13 @@ const VideoSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
-    }
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
 });
 
 const model = mongoose.model("Video", VideoSchema);
