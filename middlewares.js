0f8dd9a3ec162e1c3feb16bb6ff8534eb6da02fb
Jane Lee <Yesdoing13@gmail.com>
2019-05-28 17:59:32 +0900

    fix: fix scripts & feat: add footer & login styles

--- middlewares.js
+++ middlewares.js
@@ -8,7 +8,7 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.routes = routes;
     res.locals.user = {
         id: 1,
-        isAuthenticated: true
+        isAuthenticated: false
     }
     next();
 }
