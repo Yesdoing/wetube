3ad76ec65e98785d76a9e79c77539af2b7e9dc06
Jane Lee <Yesdoing13@gmail.com>
2019-06-13 23:34:38 +0900

    feat: add style for User Profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -121,7 +121,7 @@ export const userDetail = async (req, res) => {
 }
   
 
-export const editProfile = (req, res) =>
+export const getEditProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "EditProfile" });
 
 export const changePassword = (req, res) =>
