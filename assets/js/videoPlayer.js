505032b0312b16c4af64db2e8be8270d893ae76e
Jane Lee <Yesdoing13@gmail.com>
2019-06-18 18:24:21 +0900

    feat: add browser compatibility

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeButton");
+const fullScreenBtn = document.getElementById("jsFullScreen")
 
 function handlePlayClick() {
     if(videoPlayer.paused) {
@@ -18,9 +19,39 @@ function handleVolumeClick() {
     volumeBtn.innerHTML = videoPlayer.muted ? '<i class="fas fa-volume-mute"></i>' : '<i class="fas fa-volume-up"></i>'
 }
 
+function exitFullScreenClick() {
+    fullScreenBtn.innerHTML = `<i class="fas fa-expand"></i>`;
+    fullScreenBtn.addEventListener("click", goFullScreenClick);
+    if(document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if(document.webkitExitFullScreen) {
+        document.webkitExitFullScreen();
+    } else if (document.msExitFullScreen) {
+        document.msExitFullScreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    }
+}
+
+function goFullScreenClick() {
+    if(videoContainer.requestFullscreen) {
+        videoContainer.requestFullscreen();
+    } else if(videoContainer.mozRequestFullScreen) {
+        videoContainer.mozRequestFullScreen();
+    } else if (videoContainer.webkitRequestFullScreen) {
+        videoContainer.webkitRequestFullScreen();
+    } else if (videoContainer.msRequestFullScreen) {
+        videoContainer.msRequestFullScreen();
+    }
+    fullScreenBtn.innerHTML = `<i class="fas fa-compress"></i>`;
+    fullScreenBtn.removeEventListener("click", goFullScreenClick);
+    fullScreenBtn.addEventListener("click", exitFullScreenClick);
+}
+
 function init() {
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
+    fullScreenBtn.addEventListener("click", goFullScreenClick);
 }
 
 if (videoContainer) {
