e43e61b7a6e2bf2727b27e57d43f8063ff51cbc5
Jane Lee <Yesdoing13@gmail.com>
2019-06-21 20:30:24 +0900

    feat: add custom volume input in VideoPlayer

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,7 +2,10 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeButton");
-const fullScreenBtn = document.getElementById("jsFullScreen")
+const fullScreenBtn = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const volumeRange = document.getElementById("jsVolume");
 
 function handlePlayClick() {
     if(videoPlayer.paused) {
@@ -17,7 +20,40 @@ function handlePlayClick() {
 function handleVolumeClick() {
     videoPlayer.muted = !videoPlayer.muted; 
     volumeBtn.innerHTML = videoPlayer.muted ? '<i class="fas fa-volume-mute"></i>' : '<i class="fas fa-volume-up"></i>'
+    volumeRange.value = videoPlayer.muted ? 0 : videoPlayer.volume;
 }
+
+const formatDate = seconds => {
+    const secondsNumber = parseInt(seconds, 10);
+    let hours = Math.floor(secondsNumber / 3600);
+    let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+    let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+    if(hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    if(minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    if(seconds < 10) {
+        totalSeconds = `0${totalSeconds}`;
+    }
+
+    return `${hours}:${minutes}:${totalSeconds}`;
+}
+
+function getCurrentTime() {
+    currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));
+}
+
+function setTotalTime() {
+    const totalTimeString = formatDate(videoPlayer.duration);
+    totalTime.innerHTML = totalTimeString;
+    setInterval(getCurrentTime, 1000);
+}
+
 
 function exitFullScreenClick() {
     fullScreenBtn.innerHTML = `<i class="fas fa-expand"></i>`;
@@ -48,10 +84,36 @@ function goFullScreenClick() {
     fullScreenBtn.addEventListener("click", exitFullScreenClick);
 }
 
+function handleEnded() {
+    videoPlayer.currentTime = 0;
+    playBtn.innerHTML = `<i class="fas fa-play"></i>`;
+}
+
+function handleDrag(event) {
+    
+    const {
+        target: { value } 
+    } = event;
+
+    videoPlayer.volume = value;
+
+    if(value >= 0.6) {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>'
+    } else if(value >= 0.3) {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-down"></i>'
+    } else {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-off"></i>'
+    }
+}
+
 function init() {
+    videoPlayer.volume = 0.5;
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
     fullScreenBtn.addEventListener("click", goFullScreenClick);
+    videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+    videoPlayer.addEventListener("ended", handleEnded);
+    volumeRange.addEventListener("input", handleDrag);
 }
 
 if (videoContainer) {
